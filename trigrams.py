9fb4595403d8922d36ee429bd8eefdc661c79554
Mark Reynoso <mreynoso@spu.edu>
2017-10-19 17:46:59 -0700

    started iteration over dictionary

--- trigrams.py
+++ trigrams.py
@@ -2,14 +2,16 @@
 
 
 import io
+import random
 import string
 
 
 def main(path):
     f = io.open(path, encoding='utf-8')
     book = f.read().replace('\n', ' ').lower()
+    new_book = ' '.join(word.strip(string.punctuation) for word in book.split())
     f.close()
-    word_list = (book.split(' '))
+    word_list = (new_book.split(' '))
     the_dictionary = {}
     for i in range(len(word_list)-2):
         a_key = "{} {}".format(word_list[i], word_list[i+1])
@@ -18,7 +20,14 @@ def main(path):
             the_dictionary[a_key].append(a_value)
         else:
             the_dictionary[a_key] = [a_value]
-    print (the_dictionary)
+
+    output = []
+    start_key = random.choice(list(the_dictionary.keys()))
+    value = random.choice(the_dictionary[start_key])
+    output = output + start_key.split()
+    output.append(value)
+    # output_string = ' '.join(output)
+    print(output)
 
 
 
